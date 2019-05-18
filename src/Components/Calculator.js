df4a6bfe1ba7de1e8e3b1daa77e632d7d1b0e9e4
kikeanaya <luiseanayam@gmail.com>
2019-05-18 15:47:33 +0200

    started implementing mobile css

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -69,39 +69,30 @@ class Calculator extends Component {
     render() {
         return (
         <div className="calculator">
-            <div className="screen">
-                <p>{this.state.screen}</p>
-            </div>
-            <div className="keypad">
-                <button name="delete" onClick={e => this.onClick(e.target.name)}>CE</button>
-
-                <button name="/" onClick={e => this.onClick(e.target.name)}>÷</button>
-                <button name="*" onClick={e => this.onClick(e.target.name)}>x</button>
-
-                <button name="backspace" onClick={e => this.onClick(e.target.name)}>C</button>
-
-
-                <button name="7" onClick={e => this.onClick(e.target.name)}>7</button>
-                <button name="8" onClick={e => this.onClick(e.target.name)}>8</button>
-                <button name="9" onClick={e => this.onClick(e.target.name)}>9</button>
-                <button name="+" onClick={e => this.onClick(e.target.name)}>+</button>
-
-                <button name="4" onClick={e => this.onClick(e.target.name)}>4</button>
-                <button name="5" onClick={e => this.onClick(e.target.name)}>5</button>
-                <button name="6" onClick={e => this.onClick(e.target.name)}>6</button>
-                <button name="-" onClick={e => this.onClick(e.target.name)}>-</button>
-
-                <button name="1" onClick={e => this.onClick(e.target.name)}>1</button>
-                <button name="2" onClick={e => this.onClick(e.target.name)}>2</button>
-                <button name="3" onClick={e => this.onClick(e.target.name)}>3</button>                
-                <button name="." onClick={e => this.onClick(e.target.name)}>.</button>
-
-
-
-                <button className="big" name="0" onClick={e => this.onClick(e.target.name)}>0</button>
-                <button className="big"  name="result" onClick={e => this.onClick(e.target.name)}>=</button>
-
-
+            <div className= "calculator-container">
+                <div className="screen">
+                    <p>{this.state.screen}</p>
+                </div>
+                <div className="keypad">
+                    <button name="delete" onClick={e => this.onClick(e.target.name)}>CE</button>
+                    <button name="/" onClick={e => this.onClick(e.target.name)}>÷</button>
+                    <button name="*" onClick={e => this.onClick(e.target.name)}>x</button>
+                    <button name="backspace" onClick={e => this.onClick(e.target.name)}>C</button>
+                    <button name="7" onClick={e => this.onClick(e.target.name)}>7</button>
+                    <button name="8" onClick={e => this.onClick(e.target.name)}>8</button>
+                    <button name="9" onClick={e => this.onClick(e.target.name)}>9</button>
+                    <button name="+" onClick={e => this.onClick(e.target.name)}>+</button>
+                    <button name="4" onClick={e => this.onClick(e.target.name)}>4</button>
+                    <button name="5" onClick={e => this.onClick(e.target.name)}>5</button>
+                    <button name="6" onClick={e => this.onClick(e.target.name)}>6</button>
+                    <button name="-" onClick={e => this.onClick(e.target.name)}>-</button>
+                    <button name="1" onClick={e => this.onClick(e.target.name)}>1</button>
+                    <button name="2" onClick={e => this.onClick(e.target.name)}>2</button>
+                    <button name="3" onClick={e => this.onClick(e.target.name)}>3</button>                
+                    <button name="." onClick={e => this.onClick(e.target.name)}>.</button>
+                    <button className="big" name="0" onClick={e => this.onClick(e.target.name)}>0</button>
+                    <button className="big"  name="result" onClick={e => this.onClick(e.target.name)}>=</button>
+                </div>
             </div>
         </div>
         )
