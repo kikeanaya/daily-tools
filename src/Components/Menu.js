81678b014f11aefe92fddd055a973e2cbf9028d5
kikeanaya <luiseanayam@gmail.com>
2019-05-18 17:07:46 +0200

    added icons for mobile view

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -10,10 +10,10 @@ const Menu = () => {
                 <img src={logo} alt="logo"></img>
                 <h1>DAILY TOOLS</h1>
             </div>
-            <Link to={`/daily-tools/`}><h3>ABOUT</h3></Link>
-            <Link to={`/daily-tools/calculator`}><h3>CALCULATOR</h3></Link>
-            <Link to={`/daily-tools/chronometer`}><h3>CHRONOMETER</h3></Link>
-            <h3>CALENDAR</h3>
+            <Link to={`/daily-tools/`}><h3 className="about-logo">ABOUT</h3></Link>
+            <Link to={`/daily-tools/calculator`}><h3 className="calculator-logo">CALCULATOR</h3></Link>
+            <Link to={`/daily-tools/chronometer`}><h3 className="chronometer-logo">CHRONOMETER</h3></Link>
+            <h3 className="calendar-logo">CALENDAR</h3>
         </div>
     )
 }
